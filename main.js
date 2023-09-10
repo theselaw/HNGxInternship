ff2b2bc04beb052dff1e6165a0fce533e26930a9
Moses Iseoluwa Omosaye <77594582+theselaw@users.noreply.github.com>
2023-09-10 05:59:29 -0700

    Update main.js to render real - time

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ const daysOfWeek=['sunday', 'Monday','Tuesday','Wednesday','Thursday', 'Friday' 
 const currentDate = new Date ();
 const currentDay=daysOfWeek[currentDate.getDay()];
 document.querySelector('[data-testid="currentDayOfTheWeek"]').innerHTML= currentDay;
+
+function updateCurrentTime() {
 const currentUTCTime= Date.now();
-
-document.querySelector('[data-testid="currentUTCTime"]').textContent = currentUTCTime;
-
+document.querySelector('[data-testid="currentUTCTime"]').textContent = currentUTCTime.toString();
+}
 updateCurrentTime()
 setInterval(updateCurrentTime, 1000);
