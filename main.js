0da50a01bead8240f1d7a88799412a8f98c5b490
Moses Iseoluwa Omosaye <77594582+theselaw@users.noreply.github.com>
2023-09-10 02:12:09 +0000

    add codebase to base files

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-const currentDate = new Date();
-const currentDay = daysOfWeek[currentDate.getDay()];
+const daysOfWeek=['sunday', 'Monday','Tuesday','Wednesday','Thursday', 'Friday' , 'Saturday'];
+const currentDate = new Date ();
+const currentDay=daysOfWeek[currentDate.getDay()];
+document.querySelector('[data-testid="currentDayOfTheWeek"]').innerHTML= currentDay;
+const currentUTCTime= Date.now();
 
-document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = currentDay;
-
-const currentUTC = new Date().toUTCString();
-document.querySelector('[data-testid="currentUTCTime"]').textContent = currentUTC;
+document.querySelector('[data-testid="currentUTCTime"]').textContent = currentUTCTime;
